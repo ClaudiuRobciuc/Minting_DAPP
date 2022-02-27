a2ee758df976d7ea3afb6e731dd93cb1467e1178
Claudiu Robciuc <claudiu1robciuc@yahoo.com>
2022-02-27 10:27:06 +0100

    fix: favicon

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="icon" href="./favicon_fehu.png.png" />
+    <link rel="icon" href="{{ asset('storage/images/favicon_fehu.png') }}" />
     <link rel="stylesheet" href="{{ asset('css/main.css') }}" />
 
     <link rel="apple-touch-icon" href="{{ asset('storage/images/favicon_fehu.png') }}" />
